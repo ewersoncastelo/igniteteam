8e6b7a2b213eba27c0e6bc6bfe2d6bdea05c4cb3
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2023-10-16 00:44:31 -0400

    chore: add font custom

--- App.tsx
+++ App.tsx
@@ -3,10 +3,22 @@ import { ThemeProvider } from "styled-components";
 
 import theme from "@theme/index";
 
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups title="Hello World" />
+      {fontsLoaded ? <Groups title="Hello World" /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
