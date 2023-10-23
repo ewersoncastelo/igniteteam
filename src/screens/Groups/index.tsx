d6b2170f2dec11f42264e781c505c96beae41499
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2023-10-22 21:41:50 -0400

    chore: create list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,45 @@
-import React from "react-native";
+import React, { FlatList } from "react-native";
 
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 
+import { useState } from "react";
+import { ListEmpty } from "@components/ListEmpty";
+
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera dos aventureiros",
+    "Desbravadores",
+    "Jovens Geração 566",
+  ]);
+
   return (
     <Container>
       <Header />
 
-      <Highlight title="Grupos" subtitle="Reúna seu grupo" />
+      <Highlight 
+        title="Grupos" 
+        subtitle="Reúna seu grupo" 
+      />
 
-      <GroupCard title="Galera dos Aventureiros" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+            onPress={() => {}} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Opa...sem nenhum grupo por aqui" 
+          />
+        )}
+      />
     </Container>
   );
 }
